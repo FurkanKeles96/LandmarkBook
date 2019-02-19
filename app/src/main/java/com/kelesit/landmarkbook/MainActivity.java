bdd9ed7483c3783abd9e5af99b3938d05d2d5ba4
Furkan Keleş <furkankeles96@hotmail.com>
2019-02-19 10:06:52 +0300

    Landmark Book

--- app/src/main/java/com/kelesit/landmarkbook/MainActivity.java
+++ app/src/main/java/com/kelesit/landmarkbook/MainActivity.java
@@ -1,7 +1,11 @@
 package com.kelesit.landmarkbook;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -23,10 +27,28 @@ public class MainActivity extends AppCompatActivity {
         landmarkNames.add("Kız Kulesi");
         landmarkNames.add("London Eye");
 
+        Bitmap pisa = BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.pisa);
+        Bitmap eiffel = BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.eiffel);
+        Bitmap kizKulesi = BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.kizkulesi);
+        Bitmap londonEye = BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.londoneye);
 
+        ArrayList<Bitmap> landmarkImages = new ArrayList<>();  //Integer dizisi tutlarak da R.drawable kısmından görsel alınabilir.
+        landmarkImages.add(pisa);
+        landmarkImages.add(eiffel);
+        landmarkImages.add(kizKulesi);
+        landmarkImages.add(londonEye);
 
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, landmarkNames);
 
         listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+
+
+            }
+        });
     }
 }
