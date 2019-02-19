f76baeae43589f4a1350e6d01d99b6afc55708f5
Furkan Keleş <furkankeles96@hotmail.com>
2019-02-19 10:58:46 +0300

    Update MainActivity.java

--- app/src/main/java/com/kelesit/landmarkbook/MainActivity.java
+++ app/src/main/java/com/kelesit/landmarkbook/MainActivity.java
@@ -1,5 +1,6 @@
 package com.kelesit.landmarkbook;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
@@ -14,29 +15,22 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         listView = findViewById(R.id.listView);
+        final ArrayList<String> landmarkNames = new ArrayList<>();
 
-        ArrayList<String> landmarkNames = new ArrayList<>();
         landmarkNames.add("Pisa");
         landmarkNames.add("Eiffel");
         landmarkNames.add("Kız Kulesi");
         landmarkNames.add("London Eye");
 
-        Bitmap pisa = BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.pisa);
-        Bitmap eiffel = BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.eiffel);
-        Bitmap kizKulesi = BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.kizkulesi);
-        Bitmap londonEye = BitmapFactory.decodeResource(getApplicationContext().getResources(),R.drawable.londoneye);
 
-        ArrayList<Bitmap> landmarkImages = new ArrayList<>();  //Integer dizisi tutlarak da R.drawable kısmından görsel alınabilir.
-        landmarkImages.add(pisa);
-        landmarkImages.add(eiffel);
-        landmarkImages.add(kizKulesi);
-        landmarkImages.add(londonEye);
 
         ArrayAdapter arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, landmarkNames);
 
@@ -46,8 +40,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-
-
+                Intent intent = new Intent(getApplicationContext(), DetailActivity.class);
+                intent.putExtra("name", landmarkNames.get(position));
+                intent.putExtra("position",position);
+                startActivity(intent);
             }
         });
     }
